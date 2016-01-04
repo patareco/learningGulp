08d09556c4f0f75423980952eae817433915bb91
Zé Mi <zemiguelsantos@gmail.com>
2016-01-04 22:23:15 +0000

    added json crushing!

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	minifyHTML = require('gulp-minify-html'),
+	jsonMinify = require('gulp-jsonminify'),
 	concat = require('gulp-concat');
 
 var env, 
@@ -77,7 +78,7 @@ gulp.task('watch', function(){
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 	gulp.watch('builds/development/*.html', ['html']);
-	gulp.watch(jsonSources, ['json']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 
@@ -96,7 +97,9 @@ gulp.task('html', function(){
 });
 
 gulp.task('json', function(){
-	gulp.src(jsonSources)
+	gulp.src('builds/development/js/*.json')
+	.pipe(gulpif(env === 'production', jsonMinify()))
+	.pipe(gulpif(env === 'production', gulp.dest(outputDir + 'js')))
 	.pipe(connect.reload())
 });
 
