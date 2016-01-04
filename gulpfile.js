7cbbfa40452191f5c75d5011e157576d763ed5b9
Zé Mi <zemiguelsantos@gmail.com>
2016-01-04 20:02:56 +0000

    added gulpif and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
 	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat');
 
 var env, 
@@ -51,6 +53,7 @@ gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('scripts.js'))
 		.pipe(browserify())
+		.pipe(gulpif(env === 'production', uglify()))
 		.pipe(gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 });
